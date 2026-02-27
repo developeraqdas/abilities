efe084100c8ab8fa76d5a9b370e586e12013bb13
Muhammad Abu Bakar <313313hmabubakar313@users.noreply.github.com>
2026-02-27 22:58:17 +0500

    Remove raw open() usage

--- community/tripadvisor-restaurant-finder/main.py
+++ community/tripadvisor-restaurant-finder/main.py
@@ -1,5 +1,4 @@
 import json
-import os
 import re
 from typing import List, Optional
 
@@ -78,17 +77,6 @@ class TripAdvisorRestaurantCapability(MatchingCapability):
     worker: AgentWorker = None
     capability_worker: CapabilityWorker = None
     initial_request: Optional[str] = None
-
-    @classmethod
-    def register_capability(cls) -> "MatchingCapability":
-        with open(
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")
-        ) as file:
-            data = json.load(file)
-        return cls(
-            unique_name=data["unique_name"],
-            matching_hotwords=data["matching_hotwords"],
-        )
 
     # Do not change following tag
     # {{register capability}}
--- community/tripadvisor-restaurant-finder/main_minimal.py
+++ community/tripadvisor-restaurant-finder/main_minimal.py
@@ -1,5 +1,4 @@
 import json
-import os
 from src.agent.capability import MatchingCapability
 from src.agent.capability_worker import CapabilityWorker
 from src.main import AgentWorker
@@ -12,17 +11,6 @@ from src.main import AgentWorker
 class AaaCapability(MatchingCapability):
     worker: AgentWorker = None
     capability_worker: CapabilityWorker = None
-
-    @classmethod
-    def register_capability(cls) -> "MatchingCapability":
-        with open(
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")
-        ) as file:
-            data = json.load(file)
-        return cls(
-            unique_name=data["unique_name"],
-            matching_hotwords=data["matching_hotwords"],
-        )
 
     # Do not change following tag
     # {{register capability}}
